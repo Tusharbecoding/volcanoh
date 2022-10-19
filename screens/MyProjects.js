900ec6c54214d49423429a42929f2193c21291a5
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-10-19 11:52:17 +0530

    changed myprojects and ui of start project

--- screens/MyProjects.js
+++ screens/MyProjects.js
@@ -29,9 +29,9 @@ const MyProjects = ({navigation}) => {
             <Image source={{uri: 'https://firebasestorage.googleapis.com/v0/b/volcanoh-3ca19.appspot.com/o/images%2Fb570b267-5a8f-45de-9b99-7643bd8efbec?alt=media&token=5477e80b-4257-49bd-bcf4-59dc21a89358'}} style={styles.image} />
         </View>
         <View style={styles.imageRow}>
-            <Image source={require('../assets/illustration2.png')} style={styles.image} />
-            <Image source={require('../assets/illustration2.png')} style={styles.image} />
-            <Image source={require('../assets/illustration2.png')} style={styles.image} />
+        <Image source={{uri: 'https://firebasestorage.googleapis.com/v0/b/volcanoh-3ca19.appspot.com/o/images%2Ff9f4821b-e769-42c1-8b5f-5b29b4c1b465?alt=media&token=82db9d74-6d4d-404c-a063-e164c5174289'}} style={styles.image}/>
+        <Image source={{uri: 'https://firebasestorage.googleapis.com/v0/b/volcanoh-3ca19.appspot.com/o/images%2F561377cd-59f1-47c3-a2a7-a79717e7bbba?alt=media&token=00ae5db1-9d54-4a98-b5bd-89345492e69f'}} style={styles.image} />
+        <Image source={{uri: 'https://firebasestorage.googleapis.com/v0/b/volcanoh-3ca19.appspot.com/o/images%2Fd5a8fda0-e520-4fcd-a04a-c1cc290e38c5?alt=media&token=79693b55-c194-4af2-b0b1-01f436b125e3'}} style={styles.image} />
         </View>
       </View>
       </ScrollView>
--- screens/StartProject.js
+++ screens/StartProject.js
@@ -15,10 +15,11 @@ import {StyleSheet} from 'react-native';
 const auth = getAuth();
 const Drawer = createDrawerNavigator();
 
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 const StartProject = ({route, navigation}) => {
-  const windowWidth = Dimensions.get('window').width;
-  const windowHeight = Dimensions.get('window').height;
+  
   const { user } = useAuthentication();
 
   const [image, setImage] = useState(null);
@@ -28,7 +29,7 @@ const StartProject = ({route, navigation}) => {
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
+    let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
@@ -44,39 +45,7 @@ const StartProject = ({route, navigation}) => {
     
   };
 
-  const styles = StyleSheet.create({
-    leftBtn:{
-      bottom: windowWidth * -0.145,
-      right: windowWidth * 0.27,
-      paddingTop: 10,
-      paddingBottom: 10,
-      paddingLeft: windowWidth * 0.2,  
-      paddingRight: windowWidth * 0.27,
-      color: 'blue'
-    },
-    rightBtn:{
-      top: windowHeight * 0.019 ,
-      left: windowWidth * 0.27,
-      paddingTop: 10,
-      paddingBottom: 10,
-      paddingLeft: windowWidth * 0.27,  
-      paddingRight: windowWidth * 0.2,
-    },
-    selBtn: {
-      borderTopWidth:0,
-      borderRightWidth:0,
-      borderLeftWidth:0,
-      borderWidth: 1,
-      rolor: 'blue',
-    },
-    unselBtn: {
-      borderTopWidth:0,
-      borderLeftWidth:0,
-      borderWidth: 0,
-      
-    },
-    
-  })
+  
 
   const changeScreen = () => {
     if (image === null) {
@@ -85,62 +54,155 @@ const StartProject = ({route, navigation}) => {
     // navigation.navigate('StartProjectRight', { imageData: image });
   }
 
-  const handleToggleLeft = () => {
-    setRColor(false);
-  }
-  const handleToggleRight = () => {
-    setRColor(true);
-  }
   
-
-
-
-
+  
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    
-    <Image source={require('../assets/logo.png')} style={{
-        alignSelf: 'center',
-        width: 160,
-        height: 45,
-        top: windowHeight * 0.045,
-      }} />
-      <Octicons name="three-bars" size={25} color="rgba(16, 16, 16, 1)" style={{right: windowWidth * 0.42, top: windowHeight * 0.001}} onPress={() => navigation.openDrawer()} />
-
-      <TouchableOpacity 
-      style={[(rColor) ? styles.unselBtn : styles.selBtn, styles.leftBtn]}
-      onPress={handleToggleLeft}
-      >
-        <Text style={{color: 'black'}}
-        
-        >Left</Text>
-      </TouchableOpacity>
-      <TouchableOpacity 
-      style = {[(rColor) ? (styles.selBtn) : (styles.unselBtn), styles.rightBtn]}
-      onPress={handleToggleRight}
-      >
-        <Text
-        style={{color: 'black'}}
-        
-        >Right</Text>
-      </TouchableOpacity>
-      <Image source={require('../assets/backgroundproject.png')} style={{top: windowHeight * 0.07}}></Image>
-      <Text
-      style={{
-        fontWeight: 'bold',
-        width: 90,
-        zIndex: 1,
-        bottom: windowHeight * 0.3,
-      }}>Tap to create a new banner</Text>
-      <TouchableOpacity style={{ zIndex: 1,
-      bottom: windowHeight * 0.28,}} onPressIn={pickImage} onPress={changeScreen}>
-      <Image source={require('../assets/Group9.png')}></Image>
-      </TouchableOpacity>
-      
-    </SafeAreaView>
-    
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Octicons name="three-bars" size={27} color="black" onPress={() => navigation.openDrawer()}/>
+        <Image source={require('../assets/logo.png')} style={styles.title}/>
+        <Text></Text>
+      </View>
+      <View style={styles.leftRightHeader}>
+        <TouchableOpacity style={styles.leftTouch}>
+          <Text style={styles.left}>Left</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.rightTouch}>
+          <Text style={styles.right}>Right</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bannerContainer}>
+        <View>
+          <Image source={require('../assets/backgroundproject.png')} style={styles.banner}></Image>
+        </View>
+        <View style={styles.selectView}>
+          <Text style={{width: 90, alignItems: 'center', fontWeight: '800', marginBottom: 10, }}>Tap to create a new banner</Text>
+          <TouchableOpacity onPressIn={pickImage} onPress={changeScreen}>
+            <Image source={require('../assets/Group9.png')}></Image>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   )
-}
 
+
+
+  // return (
+  //   <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    
+  //   <Image source={require('../assets/logo.png')} style={{
+  //       alignSelf: 'center',
+  //       width: 160,
+  //       height: 45,
+  //       top: windowHeight * 0.045,
+  //     }} />
+  //     <Octicons name="three-bars" size={25} color="rgba(16, 16, 16, 1)" style={{right: windowWidth * 0.42, top: windowHeight * 0.001}} onPress={() => navigation.openDrawer()} />
+
+  //     <TouchableOpacity 
+  //     style={[(rColor) ? styles.unselBtn : styles.selBtn, styles.leftBtn]}
+  //     onPress={handleToggleLeft}
+  //     >
+  //       <Text style={{color: 'black'}}
+        
+  //       >Left</Text>
+  //     </TouchableOpacity>
+  //     <TouchableOpacity 
+  //     style = {[(rColor) ? (styles.selBtn) : (styles.unselBtn), styles.rightBtn]}
+  //     onPress={handleToggleRight}
+  //     >
+  //       <Text
+  //       style={{color: 'black'}}
+        
+  //       >Right</Text>
+  //     </TouchableOpacity>
+  //     <Image source={require('../assets/backgroundproject.png')} style={{top: windowHeight * 0.07}}></Image>
+  //     <Text
+  //     style={{
+  //       fontWeight: 'bold',
+  //       width: 90,
+  //       zIndex: 1,
+  //       bottom: windowHeight * 0.3,
+  //     }}>Tap to create a new banner</Text>
+  //     <TouchableOpacity style={{ zIndex: 1,
+  //     bottom: windowHeight * 0.28,}} onPressIn={pickImage} onPress={changeScreen}>
+  //     <Image source={require('../assets/Group9.png')}></Image>
+  //     </TouchableOpacity>
+      
+  //   </SafeAreaView>
+    
+  // )
+}
+const styles = StyleSheet.create({
+  container: {
+    height: height,
+    width: width,
+  },
+  header: {
+    display: 'flex',
+    padding: 10,
+    marginTop: 70,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    display: 'flex',
+    width: 160,
+    height: 45,
+  },
+  leftRightHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: height * 0.08,
+    
+    marginTop: 10,
+    width: width,
+  },
+  leftTouch: {
+    
+    display: 'flex',
+    height: "100%",
+    width: width * 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderBottomWidth: 1,
+  },
+  rightTouch: {
+    
+    display: 'flex',
+    height: "100%",
+    width: width * 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  left: {
+    fontSize: 18,
+  },
+  right: {
+    fontSize: 18,
+  },
+  bannerContainer: {
+    display: 'flex',
+    height: 500,
+    margin: 10,
+    marginTop: 30,
+    borderRadius: 15,
+  },
+  banner : {
+    display: 'flex',
+    height: "100%",
+    width: "100%",
+  },
+  selectView: {
+    display: 'flex',
+    position: 'absolute',
+    height: "100%",
+    width: "100%",
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
 
 export default StartProject
